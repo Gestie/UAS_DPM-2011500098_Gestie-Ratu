568f6d2588fba4dc74f52b60603a61933336c371
Gestie <gestieratuuu@gmail.com>
2022-06-22 13:23:23 +0700

    UAS DPM

---
+++ app/src/main/java/com/example/uasdpm2011500098/DataDosen.kt
@@ -0,0 +1,11 @@
+package com.example.uasdpm2011500098
+
+class DataDosen(
+    var Nidn: String,
+    var NmDosen: String,
+    var jabatan: String,
+    var GolPangkat: String,
+    var pendidikan: String,
+    var keahlian: String,
+    var ProgramStudi:String
+)
